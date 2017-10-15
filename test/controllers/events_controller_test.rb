0f70ac5e15869f52ebf43d37fd3be31f91a89496
John Olmsted <john@olmsted.io>
2017-10-15 14:44:25 -0700

    Adds integration tests for events controller

---
+++ test/controllers/events_controller_test.rb
@@ -0,0 +1,10 @@
+class EventsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @events_uri = "/events"
+  end
+
+  test "index" do
+    get @events_uri
+    assert_response :success
+  end
+end
