c2c0c70d73f70bb8739a91f954141a1207f7cac2
Kyle Schmolze <kyletns@gmail.com>
2018-05-11 14:55:10 -0700

    new event url format: /events/:id/:slug

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :events, only: [:index, :show]
+  get '/events', to: 'events#index', as: :events
+  get '/events/:id/:slug', to: 'events#show', as: :event
 
   get 'news', to: 'blog_posts#index', as: 'blog_posts'
   get 'news/:year/:month/:day/:slug', to: 'blog_posts#show', as: 'blog_post'
