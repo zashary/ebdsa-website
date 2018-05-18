9e7093734d44d1ebe470ccca9d506e8f4d8563b4
Kyle Schmolze <kyletns@gmail.com>
2018-05-17 19:19:41 -0700

    join page v1

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,4 @@
+class StaticController < ApplicationController
+  def join
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :admins, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   get '/events', to: 'events#index', as: :events
   get '/events/:id/:slug', to: 'events#show', as: :event
+
+  get 'join' => 'static#join'
 
   get 'news', to: 'blog_posts#index', as: 'blog_posts'
   get 'news/:year/:month/:day/:slug', to: 'blog_posts#show', as: 'blog_post'
