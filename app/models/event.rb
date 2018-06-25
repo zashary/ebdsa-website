0072a797796ef6b83c9a252ce11ab29e715ea898
Dominic Dagradi <dominic@chain.com>
2018-06-24 20:59:30 -0700

    Sort events returned by NationBuilder API

--- app/models/event.rb
+++ app/models/event.rb
@@ -43,22 +43,25 @@ class Event
     @nation_builder_client
   end
 
-  def self.query(start_date: Date.today, end_date: nil, limit: 1000, tags: [])
+  def self.query(start_date: Date.today, end_date: nil, limit: 500, tags: [])
     opts = {
       site_slug: ENV['NATION_SITE_SLUG'],
       starting: start_date,
-      limit: limit
+      limit: 500 # NationBuilder API doesn't return sorted events, so just grab a bunch
     }
+    opts[:until] = end_date if end_date
+
     # NationBuilder API doesnt seem to actually work
     # when passed multiple tags (it just returns all events)
     # Instead, let's just filter with tags after the API
     # call responds
-    opts[:until] = end_date if end_date
     tags = tags.clone
     other = tags.delete 'other'
 
     @events = client.call(:events, :index, opts)["results"]
       .select{ |e| ['published', 'expired'].include?(e['status']) }
+      .sort_by{ |e| e['start_time']} # NationBuilder API returns unsorted events
+      .take(limit)
       .map{ |e| Event.new(e) }
       .select{ |event|
         next true if tags.blank? and other.blank? # return all events if no tags provided
@@ -67,7 +70,6 @@ class Event
         # 'other' category - event lacks any of our listed tags, but other was checked:
         ((TAGS.keys & event.tags).blank? && other)
       }
-      # .maxListLength...
   end
 
   def self.find(id)
