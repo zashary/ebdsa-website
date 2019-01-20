0277fef1687f8c9045e76f1f4d95f35458a1060c
Dominic Dagradi <ddagradi@gmail.com>
2019-01-19 16:48:23 -0800

    redirect from not_found handler, not just PagesController (#222)

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,5 +1,11 @@
 class ErrorsController < ApplicationController
   def not_found
+    original_path = request.env['ORIGINAL_FULLPATH']
+
+    if r = (Redirect.find_by_from_path("#{original_path}") || Redirect.find_by_from_path("#{original_path}/"))
+      r.increment! :clicks
+      redirect_to r.to_url, status: :moved_permanently
+    end
   end
 
   def internal_error
