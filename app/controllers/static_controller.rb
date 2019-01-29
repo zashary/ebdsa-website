aab36bd77b2e7e69912bef542545512a6dab8d87
Dominic Dagradi <ddagradi@gmail.com>
2019-01-28 22:04:14 -0800

    Add donorbox embed (#223)

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,9 +1,7 @@
 class StaticController < ApplicationController
-  # Currently the join page is the only hard-coded static page,
-  # because we want a signup form to appear half-way down the page
-  # (as opposed to the very bottom of the page). Once we re-design
-  # the content, or build support for mid-page forms in the Page model,
-  # we can remove this.
   def join
+  end
+
+  def doante
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   post '/events/:id/rsvp', to: 'events#rsvp', as: :rsvp
 
   get 'join' => 'static#join'
+  get 'donate' => 'static#donate'
 
   get 'news', to: 'blog_posts#index', as: 'blog_posts'
   get 'news/:year/:month/:day/:slug', to: 'blog_posts#show', as: 'blog_post'
