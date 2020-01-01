1d55723fc5c3bb89b62725060d876f614b025f65
Isaac Elias <nicecurelom@gmail.com>
2019-12-31 20:08:57 -0800

    added Majority purple subsection (#239)
    * added Majority purple subsection
    
    * removed extra space

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,7 @@ class PagesController < ApplicationController
     @posts = BlogPost.homepage.limit(3)
     @highlighted_campaigns = Page.highlighted_campaigns.order(order: :asc).limit(3)
     @show_campaigns_homepage_section = Flipper.enabled?(:campaigns_homepage_section)
+    @show_majority_homepage_section = Flipper.enabled?(:majority_homepage_section)
 
     if Setting.homepage_hardcoded
       render 'home', layout: 'full_width'
