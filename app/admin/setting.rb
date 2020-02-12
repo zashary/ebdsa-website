873bca6bf3bd848f785452f7e32d8d8c22b51968
Dominic Dagradi <ddagradi@gmail.com>
2020-02-11 19:15:33 -0800

    Add global banner w/ settings

--- app/admin/setting.rb
+++ app/admin/setting.rb
@@ -6,7 +6,12 @@ ActiveAdmin.register_page 'Setting' do
   display = {
     homepage_hardcoded: :boolean,
     homepage_image_url: :string,
-    search_in_footer: :boolean
+    search_in_footer: :boolean,
+
+    banner_a_show: :boolean,
+    banner_b_title: :string,
+    banner_c_text: :string,
+    banner_d_learn_more: :string,
   }
   active_admin_settings_page(title: title, display: display)
 end
