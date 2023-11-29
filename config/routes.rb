24818245eaca2929bb274e06a5147163f15de48c
Zashary Maskus-Lavin <zashary.maskus-lavin@oracle.com>
2023-11-28 21:51:58 -0800

    Adding split dues page Adding calculator for fundraising Improving button styling

--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   get 'join' => 'static#join'
   get 'donate' => 'static#donate'
+  get 'splitdues' => 'static#splitdues'
 
   get 'news', to: 'blog_posts#index', as: 'blog_posts'
   get 'news/:year/:month/:day/:slug', to: 'blog_posts#show', as: 'blog_post'
